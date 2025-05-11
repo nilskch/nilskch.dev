f295326f17634f6576674381520b2a50d8dc8cca
Nils Koch <mail@nilskch.dev>
2025-05-11 15:14:36 +0100

    chore: fix posthog config for static html

--- app/components/PostHogProvider.tsx
+++ app/components/PostHogProvider.tsx
@@ -8,8 +8,7 @@ import { usePathname, useSearchParams } from "next/navigation";
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: "/ingest",
-      ui_host: "https://eu.posthog.com",
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       capture_pageview: false,
       capture_pageleave: true,
       debug: process.env.NODE_ENV === "development",
--- next.config.js
+++ next.config.js
@@ -11,23 +11,6 @@ const nextConfig = {
 
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
-
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://eu-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://eu.i.posthog.com/:path*",
-      },
-      {
-        source: "/ingest/decide",
-        destination: "https://eu.i.posthog.com/decide",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
